26e5ee5691d33d3cec02c79043890996e418617a
yeppp27 <69355369+yeppp27@users.noreply.github.com>
2025-04-16 15:55:41 +0800

    Update train_mutli_stage1.sh

--- bunny/script/train_multi/train_mutli_stage1.sh
+++ bunny/script/train_multi/train_mutli_stage1.sh
@@ -1,25 +1,24 @@
 cd  /mnt/workspace/workgroup/lyt/Q-Adapt/
-MODEL_TYPE=phi-2
-MODLE_STAGE1='/mnt/data_nas/luyt/bunny_copy/Bunny/output_co1/checkpoints-phi-2/lora_co_instruct_230k_nopathway_dsw/'
+
 PRETRAIN_DIR='Bunny-v1_0-3B'
-OUTPUT_DIR='bunny_mmonly_stage1_coninstruct_230k'
-mkdir -p /mnt/data_nas/luyt/VLM_weight_output/q-adapt/checkpoints-$MODEL_TYPE/$OUTPUT_DIR
+OUTPUT_DIR='lora_co_instruct_pathway_230k'
+mkdir -p ./output/checkpoints-$MODEL_TYPE/$OUTPUT_DIR
 
 nohup deepspeed --master_port 28802 --include localhost:0,1,2,3 bunny/train/train_multiimages.py \
-    --lora_enable False --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed /mnt/data_nas/luyt/bunny_copy/Bunny/script/deepspeed/zero2.json \
-    --model_name_or_path /mnt/data_nas/luyt/bunny_copy/Bunny/weight/Bunny-v1_0-3B/ \
+    --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
+    --deepspeed bunny/script/deepspeed/zero2.json \
+    --model_name_or_path pretrained_weight/Bunny-v1_0-3B/ \
     --model_type $MODEL_TYPE \
     --version bunny \
-    --data_path /mnt/data_nas/luyt/coinstruct/coinstruct_230k.json\
-    --image_folder /mnt/data_nas/luyt/coinstruct/ \
+    --data_path data/coinstruct/coinstruct_230k.json \
+    --image_folder data/coinstruct/images/ \
     --vision_tower /path/to/siglip-so400m-patch14-384 \
     --mm_projector_type mlp2x_gelu \
     --image_aspect_ratio pad \
     --group_by_modality_length False \
     --bf16 False \
     --fp16 True \
-    --output_dir /mnt/data_nas/luyt/VLM_weight_output/q-adapt/checkpoints-$MODEL_TYPE/$OUTPUT_DIR/ \
+    --output_dir ./output/checkpoints-$MODEL_TYPE/$OUTPUT_DIR \
     --num_train_epochs 1 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
@@ -41,4 +40,4 @@ nohup deepspeed --master_port 28802 --include localhost:0,1,2,3 bunny/train/trai
     --multimodal_keywords mm_projector,vision_tower,vlm_att_encoder \
     --tune_mm_mlp_adapter True \
     --freeze_backbone True \
-> /mnt/data_nas/luyt/VLM_weight_output/q-adapt/checkpoints-$MODEL_TYPE/$OUTPUT_DIR/log.log 2>&1 &
+> VLM_weight_output/q-adapt/checkpoints-$MODEL_TYPE/$OUTPUT_DIR/log.log 2>&1 &
